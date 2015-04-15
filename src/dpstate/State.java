e3058832b775739f15abba74803871ef09a9fe44
boy20046413 <boy6413036@gmail.com>
2015-04-15 15:38:59 +0800

    StatePattern

---
+++ src/dpstate/State.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dpstate;
+
+/**
+ *
+ * @author User
+ */
+public interface State {
+   public void doAction(Context context);
+}
